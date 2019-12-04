d3ede393ba4d6fb08169cf8d6d5acb564f066588
root <santhosh.annam@devrabbit.com>
2019-12-04 11:13:22 +0530

    fxgf

---
+++ src/test/java/api/demoproject/googlemapsapi/apipageclasses/APU_TestDataset.java
@@ -0,0 +1,67 @@
+package api.demoproject.googlemapsapi.apipageclasses; 
+
+public class APU_TestDataset{
+
+public static final int datasetLength = 1; 
+
+public static String projectName = "demoproject";
+
+ public static String primaryInfo  = "{\"module_description\":\"\","
+ + "\"docker_files_directory_url\":\"https://smartqe.io:443/pdownload/DockerFiles\","
+ + "\"mobile_platform\":null,"
+ + "\"report_upload_url\":\"https://smartqe.io:443/UploadReportFile\","
+ + "\"repository_commit_message\":\"fxgf\","
+ + "\"browser_type\":null,"
+ + "\"client_timezone_id\":\"Asia/Calcutta\","
+ + "\"docker_files_directory\":\"/opt/tomcat9/webapps/pdownload/DockerFiles\","
+ + "\"project_url\":\"https://newsapi.org/v2/\","
+ + "\"jenkins_username\":\"devrabbit\","
+ + "\"testcase_id\":308,"
+ + "\"project_name\":\"DemoProject\","
+ + "\"testset_name\":null,"
+ + "\"is_web\":false,"
+ + "\"jenkins_password\":\"rabbit\","
+ + "\"testcase_overwrite\":false,"
+ + "\"docker_file_name\":\"DockerFile_7_SE_TC_308\","
+ + "\"project_id\":453,"
+ + "\"framework_path\":\"/home/devrabbit/SmartQE/SmartQE-Users/7/Selenium/DemoProject\","
+ + "\"sub_module_id\":0,"
+ + "\"repository_url\":\"1nux/Oa7/Z1uRcUZ9tUGDUYcN2hpisj6nYMmawM21BZ6IvjToxm6kPKWQr6dVPecA1xC1klvgxuYRi2lz2aXsw5G4AlecA3jAwhNFUn3XaET4kJicb5X+CMaMLfeP7gL\","
+ + "\"sub_module_description\":null,"
+ + "\"testcase_name\":\"TC_EmplyessAPI\","
+ + "\"executed_timestamp\":1575438197138,"
+ + "\"is_execute\":false,"
+ + "\"file_name\":null,"
+ + "\"aes_secret_key\":\"_SmartQE_SmartQE\","
+ + "\"testset_id\":0,"
+ + "\"jenkins_token\":\"114de69b453efd3524fce8756b1ed0e69c\","
+ + "\"is_generate\":false,"
+ + "\"jenkins_url\":\"http://192.168.1.141:8080/\","
+ + "\"project_description\":\"This is for Demo\","
+ + "\"module_id\":774,"
+ + "\"user_id\":7,"
+ + "\"bundle_id\":null,"
+ + "\"sub_module_name\":null,"
+ + "\"device_os_version\":null,"
+ + "\"execution_environment\":\"jenkins\","
+ + "\"module_name\":\"GoogleMapsAPI\","
+ + "\"executed_user_id\":7,"
+ + "\"desktop_automation\":false,"
+ + "\"input_command\":null}";
+
+
+			/*--------Default Dataset-----------*/
+
+public static final String datasetHeader1 = "TC_EmplyessAPI-Default";
+
+public static final String datasetResources1 = "employees";
+public static final int requestType1 = 1;
+public static final int bodyType1 = -1;
+
+ public static String urlParams1  = "[]";
+
+ public static String headers1  = "[]";
+
+ public static String body1  = "{}";
+
+}
